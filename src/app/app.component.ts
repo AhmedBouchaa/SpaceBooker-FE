b2fbe8a4d753d66ce3ac8b0292d2e9734d9418d3
AhmedBouchaa <ahmedbouchaa0319@gmail.com>
2024-11-16 13:46:33 +0100

    Your commit message describing the changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
 import { MainComponent } from "./main/main.component";
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MainComponent,RouterLinkActive,RouterModule],
+  imports: [RouterOutlet, MainComponent,RouterLinkActive,RouterModule,HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
